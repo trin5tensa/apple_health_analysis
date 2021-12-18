a8495a5a716ff7b75166c538755c55db92ad2e95
Stephen Rigden <stepcomfive@wowway.com>
2021-12-18 13:29:17 -0500

    Copyright messages updated

--- heart_health/utilities.py
+++ heart_health/utilities.py
@@ -1,17 +1,19 @@
 """Utility functions to support Apple Medical notebooks."""
 
-#  Copyright ©2022. Stephen Rigden.
-#  Last modified 1/1/22, 9:00 AM by stephen.
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# ##################################################################################################
+#  Copyright ©2021. Stephen Rigden.                                                                #
+#  Last modified 12/18/21, 1:29 PM by stephen.                                                     #
+#  This program is free software: you can redistribute it and/or modify                            #
+#  it under the terms of the GNU General Public License as published by                            #
+#  the Free Software Foundation, either version 3 of the License, or                               #
+#  (at your option) any later version.                                                             #
+#  This program is distributed in the hope that it will be useful,                                 #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of                                  #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                                   #
+#  GNU General Public License for more details.                                                    #
+#  You should have received a copy of the GNU General Public License                               #
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.                           #
+# ##################################################################################################
 
 from dataclasses import dataclass
 from typing import Optional, Union

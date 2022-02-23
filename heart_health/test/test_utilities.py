19b281e613e4ef4228c431c53449bb7594fa3a4c
Stephen Rigden <stepcomfive@wowway.com>
2022-02-23 07:26:57 -0500

    apple_health_analysis-44 Normalization of start date in TimeCategories
    utiities.TimeCategories has been replaced with a more flexible TimeBins class.
    
    Issue Spec Deviation:
    Integration testing showed the need for a demo External Factors file which has been added.

--- heart_health/test/test_utilities.py
+++ heart_health/test/test_utilities.py
@@ -2,7 +2,7 @@
 
 # ##################################################################################################
 #  Copyright ©2022. Stephen Rigden.                                                                #
-#  Last modified 1/22/22, 8:57 AM by stephen.                                                      #
+#  Last modified 2/23/22, 7:26 AM by stephen.                                                      #
 #  This program is free software: you can redistribute it and/or modify                            #
 #  it under the terms of the GNU General Public License as published by                            #
 #  the Free Software Foundation, either version 3 of the License, or                               #
@@ -18,144 +18,228 @@ import datetime
 from contextlib import contextmanager
 
 import pytest
+
 from heart_health import utilities
 
+
 class TestTimeCategory:
+    
     @contextmanager
     def three_category_context(self):
+        """
+        Set up a ten day inclusive date range which will produce three buckets:
+        2021-12-5, 2021-12-2, and 2021-11-29. 2021-11-26 is discarded.
+        """
         start = utilities.pandas.Timestamp(2021, 11, 26)
         end = utilities.pandas.Timestamp(2021, 12, 5)
-        category_size = 3
-        time_categories = utilities.TimeCategories(start, end, bin_size=category_size)
-        yield time_categories
-        
+        bin_size = 3
+        time_bins = utilities.TimeBins(start_date=start, end_date=end, bin_size=bin_size)
+        yield time_bins
+    
     @contextmanager
     def twelve_day_context(self, bin_count: int):
+        """
+        Set up a date range with a caller specified bin count. A twelve days is requested but the exact range is
+        determined by the bin size.
+        """
         one_day = utilities.pandas.Timedelta('1 day')
         length = utilities.pandas.Timedelta('12 day')
         start = utilities.pandas.Timestamp(2022, 1, 1)
         end = start + length - one_day
-        time_categories = utilities.TimeCategories(start, end, bin_count=bin_count)
-        yield time_categories
-        
-    def test_too_large_date_returns_none(self):
+        time_bins = utilities.TimeBins(start_date=start, end_date=end, bin_count=bin_count)
+        yield time_bins
+    
+    def test_late_date_returns_none(self):
         date = utilities.pandas.Timestamp(2021, 12, 6)
-        with self.three_category_context() as time_categories:
-            assert time_categories.get_category(date) is None
-
-    def test_too_small_date_returns_none(self):
+        with self.three_category_context() as time_bins:
+            assert time_bins.get_bin(date) is None
+    
+    def test_early_date_returns_none(self):
         date = 2021, 10, 6
-        with self.three_category_context() as time_categories:
-            assert time_categories.get_category(date) is None
-            
-    def test_date_in_invalid_category(self):
-        start = utilities.pandas.Timestamp(2021, 12, 10)
-        end = utilities.pandas.Timestamp(2021, 12, 15)
-        category_size = 4
-        time_categories = utilities.TimeCategories(start, end, bin_size=category_size)
-        date = 2021, 12, 10
-        assert time_categories.get_category(date) is None
-
-    def test_date_not_in_invalid_category(self):
-        start = utilities.pandas.Timestamp(2021, 12, 10)
-        end = utilities.pandas.Timestamp(2021, 12, 15)
-        category_size = 6
-        time_categories = utilities.TimeCategories(start, end, bin_size=category_size)
-        date = 2021, 12, 10
-        assert time_categories.get_category(date) == '2021-12-15'
-        
+        with self.three_category_context() as time_bins:
+            assert time_bins.get_bin(date) is None
+    
+    def test_discarded_date_returns_none(self):
+        date = 2021, 11, 26
+        with self.three_category_context() as time_bins:
+            assert time_bins.get_bin(date) is None
+    
     def test_end_date(self):
         date = 2021, 12, 5
-        with self.three_category_context() as time_categories:
-            assert time_categories.get_category(date) == '2021-12-05'
-            
+        with self.three_category_context() as time_bins:
+            assert time_bins.get_bin(date) == '2021-12-05'
+    
     def test_2021_12_02(self):
         date = 2021, 12, 2
-        with self.three_category_context() as time_categories:
-            assert time_categories.get_category(date) == '2021-12-02'
-            
+        with self.three_category_context() as time_bins:
+            assert time_bins.get_bin(date) == '2021-12-02'
+    
     def test_2021_12_01(self):
         date = 2021, 12, 1
-        with self.three_category_context() as time_categories:
-            assert time_categories.get_category(date) == '2021-12-02'
-            
+        with self.three_category_context() as time_bins:
+            assert time_bins.get_bin(date) == '2021-12-02'
+    
     def test_2021_11_30(self):
         date = 2021, 11, 30
-        with self.three_category_context() as time_categories:
-            assert time_categories.get_category(date) == '2021-12-02'
-           
+        with self.three_category_context() as time_bins:
+            assert time_bins.get_bin(date) == '2021-12-02'
+    
     def test_2021_11_29(self):
         date = 2021, 11, 29
-        with self.three_category_context() as time_categories:
-            assert time_categories.get_category(date) == '2021-11-29'
-            
+        with self.three_category_context() as time_bins:
+            assert time_bins.get_bin(date) == '2021-11-29'
+    
     def test_normalized_start_date(self):
         date = 2021, 11, 27
-        with self.three_category_context() as time_categories:
-            assert time_categories.get_category(date) == '2021-11-29'
-        
+        with self.three_category_context() as time_bins:
+            assert time_bins.get_bin(date) == '2021-11-29'
+    
     def test_end_date_lt_start_date_raises_value_error(self):
         start = utilities.pandas.Timestamp(2021, 11, 8)
         end = utilities.pandas.Timestamp(2021, 11, 7)
-        category_size = 10
         
         expected = f'The start date {start} must precede the end date {end}.'
         with pytest.raises(ValueError) as cm:
-            utilities.TimeCategories(start, end, bin_size=category_size)
+            utilities.TimeBins(start_date=start, end_date=end)
         assert str(cm.value) == expected
-       
-    def test_invalid_category_size_raises_value_error(self):
-        start = utilities.pandas.Timestamp(2021, 11, 8)
-        end = utilities.pandas.Timestamp(2021, 11, 10)
-        category_size = 42
-        
-        expected = (f"The bin_size of 42 days must be less than the time span between "
-                    f"the start and end dates (3 days).")
-        with pytest.raises(ValueError) as cm:
-            utilities.TimeCategories(start, end, bin_size=category_size)
-        assert str(cm.value) == expected
-      
+    
     def test_invalid_date_type_raises_type_error(self):
         date = 'garbage'
         expected = "Valid date types are pandas.Timestamp or tuple('YYYY', 'MM', 'DD')."
         with pytest.raises(TypeError) as cm:
-            with self.three_category_context() as time_categories:
-                time_categories.get_category(date)
-        
+            with self.three_category_context() as time_bins:
+                time_bins.get_bin(date)
         assert str(cm.value) == expected
-        
+    
     def test_default_category_size_creates_a_category_count_of_ten(self):
         start = utilities.pandas.Timestamp(2021, 11, 1)
         end = utilities.pandas.Timestamp(2021, 11, 20)
-    
-        categories = utilities.TimeCategories(start, end)
+        
+        categories = utilities.TimeBins(start_date=start, end_date=end)
         assert categories.bin_size == 2
-
-    def test_both_bin_size_and_count_raise_value_error(self):
-        start = utilities.pandas.Timestamp(2021, 11, 8)
-        end = utilities.pandas.Timestamp(2021, 12, 7)
-        bin_size = 1
-        bin_count = 1
     
-        expected = f'Supply either bin_size (1) or bin_count (1) but not both.'
+    def test_one_bin(self):
+        with self.twelve_day_context(bin_count=1) as time_bins:
+            assert time_bins.bin_size == 12
+    
+    def test_three_bins(self):
+        with self.twelve_day_context(bin_count=3) as time_bins:
+            assert time_bins.bin_size == 4
+    
+    def test_twelve_bins(self):
+        with self.twelve_day_context(bin_count=12) as time_bins:
+            assert time_bins.bin_size == 1
+
+
+class TestTimeBins:
+    start = utilities.pandas.Timestamp(2022, 1, 1)
+    end = utilities.pandas.Timestamp(2022, 1, 12)
+    bin_size = 2
+    bin_count = 6
+    default_bin_count = 10
+    
+    # No bin size tests
+    
+    def test_start_and_end_arguments(self):
+        time_bins = utilities.TimeBins(start_date=self.start, end_date=self.end)
+        assert time_bins.start_date == utilities.pandas.Timestamp(2022, 1, 3)
+        assert time_bins.end_date == self.end
+        assert time_bins.bin_size == 1
+        assert time_bins.bin_count == self.default_bin_count
+    
+    def test_start_end_and_bin_count_arguments(self):
+        bin_count = 7
+        time_bins = utilities.TimeBins(start_date=self.start, end_date=self.end, bin_count=bin_count)
+        assert time_bins.start_date == utilities.pandas.Timestamp(2022, 1, 6)
+        assert time_bins.end_date == self.end
+        assert time_bins.bin_size == 1
+        assert time_bins.bin_count == bin_count
+    
+    def test_calculated_bin_size_of_0_raises_value_error(self):
+        start = utilities.pandas.Timestamp(2022, 1, 6)
+        end = utilities.pandas.Timestamp(2022, 1, 10)
+        
+        expected = (f"The arguments have created a calculated amd invalid bin size of 0. "
+                    f"TimeBins(start_date=Timestamp('2022-01-06 00:00:00'), "
+                    f"end_date=Timestamp('2022-01-10 00:00:00'), bin_size=0, bin_count=10)")
         with pytest.raises(ValueError) as cm:
-            utilities.TimeCategories(start, end, bin_size=bin_size, bin_count=bin_count)
+            utilities.TimeBins(start_date=start, end_date=end)
+        assert str(cm.value) == expected
+    
+    # No bin count test
+    
+    def test_start_end_and_bin_size_arguments(self):
+        bin_size = 5
+        normalized_start_date = utilities.pandas.Timestamp(2022, 1, 3)
+        calculated_bin_count = 2
+        time_bins = utilities.TimeBins(start_date=self.start, end_date=self.end, bin_size=bin_size)
+        assert time_bins.start_date == normalized_start_date
+        assert time_bins.end_date == self.end
+        assert time_bins.bin_size == bin_size
+        assert time_bins.bin_count == calculated_bin_count
+
+    def test_calculated_bin_count_of_0_raises_value_error(self):
+        start = utilities.pandas.Timestamp(2022, 1, 6)
+        end = utilities.pandas.Timestamp(2022, 1, 10)
+        bin_size = 6
+    
+        expected = (f"The arguments have created a calculated amd invalid bin size of 0. "
+                    f"TimeBins(start_date=Timestamp('2022-01-06 00:00:00'), "
+                    f"end_date=Timestamp('2022-01-10 00:00:00'), bin_size=6, bin_count=0)")
+        with pytest.raises(ValueError) as cm:
+            utilities.TimeBins(start_date=start, end_date=end, bin_size=bin_size)
+        assert str(cm.value) == expected
+        
+    # No end date tests
+    
+    def test_start_and_bin_size_arguments(self):
+        bin_size = 3
+        time_bins = utilities.TimeBins(start_date=self.start, bin_size=bin_size)
+        assert time_bins.start_date == self.start
+        assert time_bins.end_date == utilities.pandas.Timestamp(2022, 1, 30)
+        assert time_bins.bin_size == bin_size
+        assert time_bins.bin_count == self.default_bin_count
+    
+    def test_start_bin_count_and_bin_size_arguments(self):
+        time_bins = utilities.TimeBins(start_date=self.start, bin_size=self.bin_size, bin_count=self.bin_count)
+        assert time_bins.start_date == self.start
+        assert time_bins.end_date == self.end
+        assert time_bins.bin_size == self.bin_size
+        assert time_bins.bin_count == self.bin_count
+    
+    # No start date tests
+    
+    def test_end_and_bin_size_arguments(self):
+        bin_size = 1
+        time_bins = utilities.TimeBins(end_date=self.end, bin_size=bin_size)
+        assert time_bins.start_date == utilities.pandas.Timestamp(2022, 1, 3)
+        assert time_bins.end_date == self.end
+        assert time_bins.bin_size == bin_size
+        assert time_bins.bin_count == self.default_bin_count
+    
+    def test_end_bin_count_and_bin_size_arguments(self):
+        time_bins = utilities.TimeBins(end_date=self.end, bin_size=self.bin_size, bin_count=self.bin_count)
+        assert time_bins.start_date == self.start
+        assert time_bins.end_date == self.end
+        assert time_bins.bin_size == self.bin_size
+        assert time_bins.bin_count == self.bin_count
+    
+    # Wrong number of parameters test
+    
+    def test_wrong_argument_count_raises_value_error(self):
+        start = utilities.pandas.Timestamp(2021, 11, 8)
+        bin_count = 43
+        expected = ("Invalid number of arguments. Supply three. If you are using the default bin_size then supply two "
+                    "others. You supplied 2. TimeBins(start_date=Timestamp('2021-11-08 00:00:00'), "
+                    "end_date=None, bin_size=None, bin_count=43).")
+        
+        with pytest.raises(ValueError) as cm:
+            utilities.TimeBins(start_date=start, bin_count=bin_count)
         assert str(cm.value) == expected
 
-    def test_one_bin(self):
-        with self.twelve_day_context(bin_count=1) as time_categories:
-            assert time_categories.bin_size == 12
-            
-    def test_three_bins(self):
-        with self.twelve_day_context(bin_count=3) as time_categories:
-            assert time_categories.bin_size == 4
-            
-    def test_twelve_bins(self):
-        with self.twelve_day_context(bin_count=12) as time_categories:
-            assert time_categories.bin_size == 1
-            
-            
+
 class TestCreateBloodPressureDataset:
+    
     @contextmanager
     def dummy_heart_dataset(self):
         """
@@ -191,9 +275,8 @@ class TestCreateBloodPressureDataset:
             pulse_pressures = utilities.pandas.Series([40, 52])
             expected = utilities.pandas.DataFrame({'date': dates, 'systolic': systolics,
                                                    'diastolic': diastolics, 'pulse pressure': pulse_pressures})
- 
             assert utilities.create_blood_pressure_dataset(bpds).compare(expected).empty
-        
+    
     def test_missing_systolic_data_raises_value_error(self):
         with self.dummy_heart_dataset() as bpds:
             bpds.type[1] = 'garbage'
@@ -203,7 +286,7 @@ class TestCreateBloodPressureDataset:
             with pytest.raises(utilities.NoDataSupplied) as cm:
                 utilities.create_blood_pressure_dataset(bpds)
             assert str(cm.value) == expected
-        
+    
     def test_missing_diastolic_data_raises_value_error(self):
         with self.dummy_heart_dataset() as bpds:
             bpds.type[0] = 'garbage'
